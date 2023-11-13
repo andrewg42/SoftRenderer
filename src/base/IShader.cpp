eba55b86f09cf9089aa192679eea6d93d297c6d4
andrewg42 <347011425@qq.com>
2023-11-13 09:31:01 +0800

    add blinn-phong

--- src/base/IShader.cpp
+++ src/base/IShader.cpp
@@ -10,11 +10,12 @@ static glm::vec3 clamp(glm::vec3 color) {
                 (color.z > 1.0f) ? 1.0f : color.z
            );
 }
-
-glm::vec3 blinn_phong_obj_color(FragShader_Payload &fsp) {
+glm::vec3 phong_obj_color(FragShader_Payload &fsp)
+{
     glm::vec3 FragColor = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 norm = glm::normalize(fsp.model_it * fsp.normal);
-    for(auto light : fsp.lights) {
+    for (auto light : fsp.lights) {
+        // ambient
         float ambientStrength = 0.1f;
         glm::vec3 ambient = ambientStrength * light.color;
 
@@ -29,6 +30,35 @@ glm::vec3 blinn_phong_obj_color(FragShader_Payload &fsp) {
         glm::vec3 reflect_dir = glm::reflect(-light_dir, norm);
         float spec =
             glm::pow(glm::max(glm::dot(view_dir, reflect_dir), 0.0f), 32.0f);
+        glm::vec3 specular = specularStrength * spec * light.color;
+
+        glm::vec3 obj_color =
+            glm::vec3(fsp.obj_color.x, fsp.obj_color.y, fsp.obj_color.z);
+        FragColor += (ambient + diffuse + specular) * obj_color;
+    }
+
+    return clamp(FragColor);
+}
+
+glm::vec3 blinn_phong_obj_color(FragShader_Payload &fsp) {
+    glm::vec3 FragColor = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 norm = glm::normalize(fsp.model_it * fsp.normal);
+    for(auto light : fsp.lights) {
+        // ambient
+        float ambientStrength = 0.1f;
+        glm::vec3 ambient = ambientStrength * light.color;
+
+        // diffuse
+        glm::vec3 light_dir = glm::normalize(light.pos - fsp.frag_pos);
+        float diff = glm::max(glm::dot(norm, light_dir), 0.0f);
+        glm::vec3 diffuse = diff * light.color;
+
+        // specular
+        float specularStrength = 0.5f;
+        glm::vec3 view_dir = glm::normalize(fsp.view_pos - fsp.frag_pos);
+        glm::vec3 reflect_dir = glm::reflect(-light_dir, norm);
+        glm::vec3 h = glm::normalize(view_dir + reflect_dir);
+        float spec = glm::pow(glm::max(glm::dot(norm, h), 0.0f), 32.0f);
         glm::vec3 specular = specularStrength * spec * light.color;
 
         glm::vec3 obj_color =
